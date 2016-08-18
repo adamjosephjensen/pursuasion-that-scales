edeba427d2568521172c92e47c61e43026b21661
Adam Jensen <adamjensen@Adams-MacBook-Pro.local>
2016-08-18 16:37:29 -0700

    bot echoes responses

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# justice-scales
+# Pursuasion that Scales
+
+This project matters because good ideas don't have enough proponents.
+
+## Setup
+
+This project requires:
+
+* python3 and pip3
+* slackclient
+
+Most installations of python3 include pip3.
+
+### On mac:
+
+```
+brew install python3
+pip3 install slackclient
+```
+
+### Ubuntu/Debian
+
+```
+sudo apt-get install -y python3
+pip3 install slackclient
+```
---
+++ bot.py
@@ -0,0 +1,43 @@
+import os
+import time
+from slackclient import SlackClient
+
+
+# starterbot's ID as an environment variable
+BOT_ID = 'U22EJ4NAD'
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+
+# instantiate Slack & Twilio clients
+slack_client = SlackClient(os.environ.get('xoxb-70494158353-8x9n5Q9Uh2eLNtQYM9MPJyb0'))
+
+
+def handle_command(command, channel):
+    print('posting %s', command)
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=command, as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                    # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel']
+    return None, None
+
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
