2f373033cb89ee8922fdb53415791177f0c3d8b1
Adam Jensen <oojensen@alumni.stanford.edu>
2016-08-21 19:30:55 -0700

    don't even get me started on keyword

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def parse_slack_output(slack_rtm_output):
 
 
 if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    READ_WEBSOCKET_DELAY = 0.1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
         while True:
